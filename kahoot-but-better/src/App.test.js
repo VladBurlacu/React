78cc160810aaa5096662db766fdf9cc085681239
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-11 09:31:16 +0200

    added 3 components

--- kahoot-but-better/src/App.test.js
+++ kahoot-but-better/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import React, {useState} from 'react';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+function Header(props) {
+
+  const [title, setTitle] = useState("Hello World!");
+
+  return (
+      <div className={"header"}>
+        <h1>{{title}}</h1>
+        <button onClick={setTitle("Hello State!")}>Click me to change state!</button>
+      </div>
+  );
+}
+
+export default Header
--- kahoot-but-better/src/index.js
+++ kahoot-but-better/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Quiz from "./Components/Quiz";
 import reportWebVitals from './reportWebVitals';
+import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Quiz />
   </React.StrictMode>
 );
 
