00b76dd5fd5642d247517e93c088962890d6704b
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-11 12:24:31 +0200

    fetched data from the api for each option in the settings

--- kahoot-but-better/src/Components/Settings.js
+++ kahoot-but-better/src/Components/Settings.js
@@ -1,12 +1,39 @@
 import SelectField from "./SelectField";
-import {Box, Button} from "@mui/material";
+import {Box, Button, CircularProgress, Typography} from "@mui/material";
 import TextFieldComp from "./TextFieldComp";
 import useAxios from "../hooks/useAxios";
 
 
 const Settings = () => {
     const { response, error, loading} = useAxios({ url: "/api_category.php" })
-    console.log(response)
+
+    if(loading) {
+        return (
+            <Box mt={20}>
+                <CircularProgress />
+            </Box>
+        )
+    }
+
+    if(error) {
+        return (
+            <Typography variant="h6" mt={20} color="red">
+               Ops! Something Went Wrong!
+            </Typography>
+        )
+    }
+
+    const difficultyOptions = [
+        { id: "easy", name: "Easy"},
+        { id: "medium", name: "Medium"},
+        { id: "hard", name: "Hard"},
+    ]
+
+    const typeOptions = [
+        { id: "multiple", name: "Multiple Choice"},
+        { id: 'boolean', name: "True/False"},
+    ]
+
     const handleSubmit = e => {
         e.preventDefault();
 
@@ -14,9 +41,9 @@ const Settings = () => {
 
     return (
         <form onSubmit={ handleSubmit }>
-            <SelectField label = "Subject"/>
-            <SelectField label = "Difficulty"/>
-            <SelectField label = "Type"/>
+            <SelectField options={response.trivia_categories} label = "Subject"/>
+            <SelectField options={difficultyOptions} label = "Difficulty"/>
+            <SelectField options={typeOptions} label = "Type"/>
             <TextFieldComp />
             <Box mt={3} width = "100%">
                 <Button fullWidth variant = "contained" type = "submit">
