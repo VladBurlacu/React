4e17a2b8a0a54044916f892fb98ea74ca021dcd5
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-11 11:15:37 +0200

    added settings options and button to start game, had some issues with mismatching versions of react and mui but fixed it

--- kahoot-but-better/src/App.js
+++ kahoot-but-better/src/App.js
@@ -6,23 +6,29 @@ import {
 import Settings from "./Components/Settings";
 import Questions from "./Components/Questions";
 import Results from "./Components/Results";
+import {Box, Container, Typography} from "@mui/material";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-          <Route path="/" exact>
-              <Settings />
-          </Route>
-          <Route path="/questions">
-              <Questions />
-          </Route>
-          <Route path="/score">
-              <Results />
-          </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Container maxWidth="sm">
+                <Box textAlign="center" mt={5}>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Typography variant="h2" fontWeight="bold">Kahoot but Better!</Typography>
+                            <Settings/>
+                        </Route>
+                        <Route path="/questions">
+                            <Questions/>
+                        </Route>
+                        <Route path="/score">
+                            <Results/>
+                        </Route>
+                    </Switch>
+                </Box>
+            </Container>
+        </Router>
+    );
 }
 
 export default App;
--- kahoot-but-better/src/Components/Settings.js
+++ kahoot-but-better/src/Components/Settings.js
@@ -1,10 +1,19 @@
-import React from "react";
+import SelectField from "./SelectField";
+import {Box, Button} from "@mui/material";
+
 
 const Settings = () => {
     return (
-        <div>
-            Settings
-        </div>
+        <form>
+            <SelectField label = "Subject"/>
+            <SelectField label = "Difficulty"/>
+            <SelectField label = "Amount of Questions"/>
+            <Box mt={3} width = "100%">
+                <Button fullWidth variant = "contained" type = "submit">
+                    Get started!
+                </Button>
+            </Box>
+        </form>
     )
 }
 
